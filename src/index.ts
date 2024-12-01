8276e55b06765426885f31d483d71128f5888ce4
xyz <xyz@xthinkpad>
2024-12-01 09:12:23 +0100

    add authentication method.

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import express, { Express } from 'express';
+import express, { Express, Request, Response, NextFunction } from 'express';
 import dotenv from 'dotenv';
 import dbmplRoutes from './dbmanuplation';
 import word3Routes from './reportword3';
@@ -7,6 +7,18 @@ dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT || 3000;
+
+const authenticateToken = (req: Request, res: Response, next: NextFunction) => {
+	const token = req.headers['authorization'];
+	if (!token) {
+		return res.status(401).json({ error: 'Unauthorized' });
+	}
+	if (token !== process.env.AUTH_TOKEN) {
+		return res.status(403).json({ error: 'Forbidden' });
+	}
+	next();
+};
+app.use(authenticateToken);
 app.use('/dbmpl', dbmplRoutes);
 app.use('/w3', word3Routes);
 app.use(express.json());
